01396d95c4b7f6f6ccbd1537ad73172dbc2d8a1b
flex-jonghyuk <jonghyuk@flex.team>
2023-09-24 23:16:23 +0900

    fastify + prom client로 서버 만들기

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import fastifyModule from "fastify";
+import { collectDefaultMetrics, register } from "prom-client";
+
+const fastify = fastifyModule({ logger: true });
+
+collectDefaultMetrics({ prefix: "nodejs_" });
+
+fastify.get("/metrics", (_, res) => {
+  res.header("Content-Type", register.contentType);
+  res.send(register.metrics());
+});
+
+fastify.get("/fibonacci/:n", (req, res) => {
+  const n = Number(req.params.n);
+  const result = fibonacci(n);
+  res.send({ result });
+});
+
+fastify.listen({ port: 3000 }, (err) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
+
+// 피보나치 수열 계산 함수
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
